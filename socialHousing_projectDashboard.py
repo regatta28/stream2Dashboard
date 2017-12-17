aca4123a1a42885374b7a6188dde71db39c2c892
Rebecca Stevens <rsbjshan@gmail.com>
2017-12-17 20:23:22 +0000

    Added MongoDb connection to flask project. defined the constant FIELDS(to retrieve the record fields I require). Retrieved the collections 'projects'. Returned the collection as a list in json format

--- socialHousing_projectDashboard.py
+++ socialHousing_projectDashboard.py
@@ -1,11 +1,30 @@
 from flask import Flask
+from flask import render_template
+from pymongo import MongoClient
+import json
 
 app = Flask(__name__)
 
+MONGODB_HOST = 'localhost'
+MONGODB_PORT = 27017
+DBS_NAME = 'socialHousing'
+COLLECTION_NAME = 'projects'
+
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template("index.html")
+
+@app.route('socialHousing/projects')
+def social_housing_projects():
+
+    FIELDS = {
+        'LA': True, 'Scheme/Project Name': True,
+    }
+    with MongoClient(MONGODB_HOST, MONGODB_PORT) as conn:
+        collection = conn[DBS_NAME][COLLECTION_NAME]
+        projects = collection.find(projection=FIELDS)
+        return json.dumps(list(projects))
 
 
 if __name__ == '__main__':
