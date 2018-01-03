807b6346d6d630a2e6889feb3a7bf55e832b36b9
Rebecca Stevens <rsbjshan@gmail.com>
2018-01-03 13:35:15 +0000

    Used crossfilter on two columns of data. Grouped the columns. Created variables for the crossfilter dimensions of the two columns and created two dc.linechart variables too the above dimensions.

--- static/js/graph.js
+++ static/js/graph.js
@@ -1,5 +1,5 @@
 queue()
-    .defer(d3.json, "/socialHousing/projects")
+    .defer(d3.json, '/socialHousing/projects')
     .await(makeGraphs);
 function makeGraphs(error, socialHousingProjects) {
     if (error) {
@@ -8,58 +8,28 @@ function makeGraphs(error, socialHousingProjects) {
     }
 
     socialHousingProjects.forEach(function (d) {
-        d["Number of Units"] = +d["Number of Units"];
-        d["Stage 1 Capital Appraisal"] = d["Stage 1 Capital Appraisal"];
-        d["Stage 2 Pre Planning"] = d["Stage 2 Pre Planning"];
-        d["Stage 3 Pre Tender design"] = d["Stage 3 Pre Tender design"];
-        d["Stage4 Tender Report or Final Turnkey/CALF approval"] = d["Stage4 Tender Report or Final Turnkey/CALF approval"];
-        d["Site Start"] = d["Site Start"];
-        d["Site Finish"] = d["Site Finish"];
         d["LA"] = d["LA"];
+        d["Number of Units"] = +d["Number of Units"];
     });
+
 
     var ndx = crossfilter(socialHousingProjects);
+    var all = ndx.groupAll();
 
-    var numberOfUnits = ndx.dimension(function (d) {
-        return d["Number of Units"];
-    });
-    var stage1 = ndx.dimension(function (d) {
-        return d["Stage 1 Capital Appraisal"];
-    });
-    var stage2 = ndx.dimension(function (d) {
-        return d["Stage 2 Pre Planning"];
-    });
-    var stage3 = ndx.dimension(function (d) {
-        return d["Stage 3 Pre Tender design"];
-    });
-    var stage4 = ndx.dimension(function (d) {
-        return d["Stage 4 Tender Report or Final Turnkey/CALF approval"];
-    });
-    var siteStart = ndx.dimension(function (d) {
-        return d["Site Start"];
-    });
-    var siteFinish = ndx.dimension(function (d) {
-        return d["Site Finish"];
-    });
-    var housesperArea = ndx.dimension(function (d) {
-        return d["LA"];
-        housesperArea.filterAll()
-        var filteredCities = housesperArea.filter (function(d) {if (d === "Galway City" || d ==="Dublin City" || d === "Cork City" || d === "Cork City") {return d;}});
-    });
+    var cityDim = ndx.dimension( function(d) { return d["LA"]; });
+    var numberOfUnitsDim = ndx.dimension( function(d) { return d["Number of Units"]; });
 
-    var numOfUnits = numberOfUnits.group();
-    var numStage1 = stage1.group();
-    var numStage2 = stage2.group();
-    var numStage3 = stage3.group();
-    var numStage4 = stage4.group();
-    var numsiteStart = siteStart.group();
-    var numsiteFinish = siteFinish.group();
-    var housesPerCity = filteredCities.group().reduceSum(function (d) {
-        return d["numberofUnits"];
-    });
+    var cityGroup = cityDim.group();
+    var numberOfUnitsGroup = numberOfUnitsDim.group();
 
-    var housePerCityChart = dc.linechart("#housesPerArea");
-    var statusPie = dc.piechart("#stageofCompl_pie-chart");
+    var cityGroupChart = dc.lineChart("#housesPerArea");
+
+    cityGroupChart
+        .dimension(cityDim)
+        .group(cityGroup);
+
+
+    dc.renderAll();
 
 
 
